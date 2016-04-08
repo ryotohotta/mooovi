3c1867c8879cf88caba472a2fc44506d14a7c5fa
Ryoto Hotta <grapevinechip@gmail.com>
2016-04-08 16:58:59 +0900

    add column to product
    add director, detail, open_date

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004013909) do
+ActiveRecord::Schema.define(version: 20160408075311) do
 
-  create_table "products", force: true do |t|
-    t.string   "title"
-    t.text     "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "reviews", force: true do |t|
-    t.string   "nickname"
-    t.integer  "rate"
-    t.text     "review"
-    t.integer  "product_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "products", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "image_url",  limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "director",   limit: 255
+    t.text     "detail",     limit: 65535
+    t.string   "open_date",  limit: 255
   end
 
 end
