d9f22ef399539596b5e230e52a276ffe1f9afcb0
Ryoto Hotta <grapevinechip@gmail.com>
2016-04-08 16:58:16 +0900

    scraping done
    scraped movie info

---
+++ app/models/scraping.rb
@@ -0,0 +1,34 @@
+class Scraping
+  def self.movie_urls
+    agent = Mechanize.new
+    links = []
+
+    next_url = "/now/"
+
+    while true do
+      current_page = agent.get("http://eiga.com" + next_url)
+      elements = current_page.search('.m_unit h3 a')
+      elements.each do |ele|
+        links << ele.get_attribute('href')
+      end
+
+      next_link = current_page.at('.next_page')
+      next_url = next_link.get_attribute('href')
+      break unless next_url
+    end
+
+    links.each do |link|
+      get_product('http://eiga.com' + link)
+    end
+  end
+
+  def self.get_product(link)
+    agent = Mechanize.new
+    page = agent.get(link)
+    title = page.at('.moveInfoBox h1').inner_text if page.at('.moveInfoBox h1')
+    image_url = page.at('.pictBox img')[:src] if page.at('.pictBox img')
+
+    product = Product.where(title: title, image_url: image_url).first_or_initialize
+    product.save
+  end
+end
