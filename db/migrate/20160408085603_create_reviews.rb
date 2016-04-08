8f7170710878c89c6ebd8c1e63fe39830b671553
Ryoto Hotta <grapevinechip@gmail.com>
2016-04-08 18:45:38 +0900

    show review done

--- db/migrate/20160408085603_create_reviews.rb
+++ db/migrate/20160408085603_create_reviews.rb
@@ -1,7 +1,10 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-
+      t.string :nickname
+      t.integer :rate
+      t.text :review
+      t.integer :product_id
       t.timestamps null: false
     end
   end
